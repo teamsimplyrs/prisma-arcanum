3a6c1cacbb10789ffc9e3d04b9a2b3ab6999b023
Belvix <16433060+Belvix@users.noreply.github.com>
2024-01-21 19:20:20 +0530

    Spell registry fixed

--- src/main/java/com/teamsimplyrs/prismaarcanum/registry/PASpellRegistry.java
+++ src/main/java/com/teamsimplyrs/prismaarcanum/registry/PASpellRegistry.java
@@ -6,16 +6,19 @@ import com.teamsimplyrs.prismaarcanum.item.spells.interitus.IgnisFireball;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.IForgeRegistry;
+import net.minecraftforge.registries.RegistryBuilder;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.util.function.Supplier;
+
 public class PASpellRegistry {
-//    public static final DeferredRegister<SpellBase> PA_SPELL = DeferredRegister.create(new ResourceLocation("spell_registry"), PrismaArcanum.MODID);
-//
-//    public static final RegistryObject<SpellBase> IGNIS_FIREBALL = PA_SPELL.register("ignis_fireball",()->new IgnisFireball());
-//
-//
-//    public static void register(IEventBus eventBus)
-//    {
-//        PA_SPELL.register(eventBus);
-//    }
+    public static final DeferredRegister<SpellBase> PA_SPELL = DeferredRegister.create(new ResourceLocation("spell_registry"), PrismaArcanum.MODID);
+    public static final Supplier<IForgeRegistry<SpellBase>> PA_SPELL_REGISTRY_SUPPLIER = PA_SPELL.makeRegistry(RegistryBuilder::new);
+    public static final RegistryObject<SpellBase> IGNIS_FIREBALL = PA_SPELL.register("ignis_fireball",()->new IgnisFireball());
+
+    public static void register(IEventBus eventBus)
+    {
+        PA_SPELL.register(eventBus);
+    }
 }
